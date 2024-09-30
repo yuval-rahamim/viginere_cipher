d4e875aa24df36b77aef8e333958b28f8e45b761
yuval-rahamim <yuval.rahamim3@gmail.com>
2024-09-30 10:36:06 +0300

    io
    added input output

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def encrypt(msg, key, upper=False):
     if upper:
         msg = msg.upper()
@@ -9,7 +12,7 @@ def encrypt(msg, key, upper=False):
     encrypted = ""
     for i in range(len(msg)):
         encrypted += chr((ord(msg[i])-ord("a") + ord(key[i % len(key)]) - ord("a")) % 26 + ord("a"))
-    return encrypted
+    return encrypted.upper()
 
 def decrypt(msg, key, upper=False):
     if upper:
@@ -27,5 +30,26 @@ def decrypt(msg, key, upper=False):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(encrypt('prosper','bop'))
-    print(decrypt(encrypt('prosper','bop'), 'bop'))
+    print("hi, what do you want to do?")
+    need = input("type enc for encrypt or dec for decrypt and exit to close the program: ").lower()
+    while need != "exit":
+        if need == "enc":
+            msg = input("please enter the msg you want to encrypt: ")
+            key = input("please enter your key: ")
+            upperString = input("is your msg and key are upper? press true or false (anything else will be lower): ").lower()
+            upper = False
+            if upperString == "true":
+                upper = True
+            print("your encrypted msg is: ",encrypt(msg, key,upper))
+        elif need == "dec":
+            msg = input("please enter the msg you want to decrypt: ")
+            key = input("please enter your key: ")
+            upperString = input("is your msg and key are upper? press true or false (anything else will be lower): ").lower()
+            upper = False
+            if upperString == "true":
+                upper = True
+            print("your encrypted msg is: ",encrypt(msg, key,upper))
+        else:
+            print("bad input.")
+        need = input("type enc for encrypt or dec for decrypt and exit to close the program: ")
+    sys.exit()
